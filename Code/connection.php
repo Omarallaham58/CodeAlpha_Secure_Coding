cc28f629f71a72a8d8730b44c304a416f5a89bce
Omarallaham58 <117765905+Omarallaham58@users.noreply.github.com>
2024-07-30 21:03:49 +0300

    Update connection.php

--- Code/connection.php
+++ Code/connection.php
@@ -1,7 +1,7 @@
 <?php
-$host="localhost";
-$username="root";
-$password="root@#$2003";
+$host="YOUR_HOST_NAME";
+$username="YOUR_DB_USERNAME";
+$password="YOUR_DB_PASSWORD";
 
 try{
     $pdo = new PDO("mysql:host=".$host.";port=3306;dbname=test_schema",$username,$password);
